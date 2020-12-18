2fcb6dfbfec55e3d0d28846bce460cd8ced5ab6b
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-12-18 16:11:29 -0600

    learned about the for of loop

--- script.js
+++ script.js
@@ -271,3 +271,15 @@ printGoals(...game.scored);
 // 7.
 team1 < team2 && console.log('Team 1 is more likely to win');
 team1 > team2 && console.log('Team 2 is more likely to win');
+
+///////////////////////////////////////
+// The for-of Loop
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for (const item of menu) console.log(item);
+
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1}: ${el}`);
+}
+
+// console.log([...menu.entries()]);
